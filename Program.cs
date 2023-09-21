aced4baf2e7901fee4123a3783759b5d9db19c00
= <=>
2023-09-21 19:06:43 +0400

    deyisiklik eledim yene

--- Program.cs
+++ Program.cs
@@ -5,106 +5,50 @@ class Program
     private static void Main(string[] args)
     {
 
-        #region
+        //Task:
+        //1.Verilmiş integer array-ini azdan choxa dogru sort edən funksiya yazın. [0, 50, -11, 23, 18]
 
-        //int end = 10;
-        //int count = 0;
+        //2.Verilmiş integer array-inin ortalamasını tapan funksiya yazın. [0, 50, -11, 23, 18 ]
 
-        //for (int number = 2; number <= end; number++)
-        //{
-        //    bool isPrime = true;
-
-        //    for (int divisor = 2; divisor*divisor <= number; divisor++)
-        //    {
-        //        if (number % divisor == 0)
-        //        {
-        //            isPrime = false;
-        //            break;
-        //        }
-        //    }
-
-        //    if (isPrime)
-        //    {
-        //        count++;
-        //    }
-        //}
-
-        //Console.WriteLine(count);
-
-        #endregion
+        //3.Verilmiş integer array-ində minimum ədədin indexsini tapan funksiya yazın.  [0, 50, -11, 23, 18 ]
 
 
-        #region
-        //int number = 1230;
-        //int count = 0;
-
-        //while (number != 0)
-        //{
-        //    number = number/10;
-        //    count++;
-        //}
-        //Console.WriteLine(count);
-        #endregion
-
-
-        #region
-        //int number = 9;
-        //int number1 = 0;
-
-        //do
-        //{
-        //    if (number1 == number)
-        //    {
-        //        Console.WriteLine("3ün qüvvətidir");
-        //        break;
-        //    }
-        //    else
-        //    {
-        //        number1 = number / 3;
-        //        //Console.WriteLine("3ün qüvvəti deyil");
-        //    }
-        //} while (number1 * number1 <= number);
-        #endregion
-
-
-        #region Less to more sort
-        int[] arr = { 0, 23, -11, 17, 18 };
-        int numi = 0;
-        int numj = 0;
-        bool test = false;
-
-
-        for (int i = 0; i < arr.Length; i++)
+        #region 2.Find middle number of array
+        static double FindMiddleNumber(int[] array)
         {
-            for (int j = 0; j < arr.Length; j++)
-            {
-                if (test)
-                {
-                    if (arr[j] < numj)
-                    {
-                        numj = arr[j];
-                        //Console.WriteLine(arr[j]);
-                    }
-                }
-                else
-                {
-                    if (arr[j] > numi && arr[j]<=numj)
-                    {
-                        numj = arr[j];
-                        //Console.WriteLine(arr[j]);
-                    }                    
-                }
-            }
-            
-            Console.WriteLine(numj);
-            test = false;
-            numi = numj;
+            double num = 0;
+            int count = 0;
 
+            for (int i = 0; i < array.Length; i++)
+            {
+                num += array[i];
+                count++;
+            }
+
+            return num / count;
         }
 
+        int[] arr = { 0, 23, -11, 17, 18 };
+        #endregion
 
-        
 
+        #region 3.Find array min value index
+        static int FindMinValIndex(int[] array)
+        {
+            int num = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] <= num)
+                {
+                    num = i;
+                }
+            }
+
+            return num;
+        }
+
+        int[] arr1 = { 0, 23, -11, 17, 18 };
         #endregion
     }
 }
